1712d0a998129944acf42ef28798c33406dc1470
asifmahmud4868-design <asifmahmud4868@gmail.com>
2026-08-19 18:18:04 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
+const CACHE = "ai-life-manager-v1";
 
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.add(new Request("./index.html", {cache:"reload"})).catch(()=>{})
+    )
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if(event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
